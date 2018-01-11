3058ad44aa1e106c7cd9ec23fa7ff16913d5f174
sapirblutman <sapirblutman@walla.com>
2018-01-11 22:43:02 +0200

    i am the queen of the gui

--- src/GameGui/Main.java
+++ src/GameGui/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("GameGui.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 500, 450));
+        primaryStage.setScene(new Scene(root, 800, 450));
         primaryStage.show();
     }
 
